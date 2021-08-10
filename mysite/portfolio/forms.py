3fabb4d20fdb249860c8d5750ddaca2248d6b89a
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2021-08-10 15:13:40 +0100

    house cleaning

--- mysite/portfolio/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Email
-
-
-class CustomerForm(ModelForm):
-    class Meta:
-        model =  Email
-        fields = ['name', 'email', 'subject', 'message']
